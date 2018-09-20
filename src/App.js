74856d1c028102d0ae2c5a22c6794533e6c20d9d
Shehan Disanayake <dmcshehan@gmail.com>
2018-09-20 12:50:08 +0530

    convert counter into a statefull component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,111 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 
+let players = [
+  {
+    name : "Shehan Disanayake",
+    score : 31,
+    id: 1
+  },
+  {
+    name : "Jim Hoskins",
+    score : 33,
+    id: 2
+  },
+  {
+    name : "ALena Holigen",
+    score : 35,
+    id : 3
+  },
+  {
+    name : "Craig Dennis",
+    score : 37,
+    id : 4
+  },
+];
+
+
+function Header(props){
+  return(
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+Header.propTypes ={
+  title : PropTypes.string.isRequired,
+};
+
+
+class Counter extends Component{
+  propTypes : {
+      initialScore : PropTypes.number.isRequired
+  }
+
+  constructor(props){
+    super(props);
+    this.state ={
+      score : this.props.initialScore
+    }
+
+    this.decrementScore = this.decrementScore.bind(this);
+    this.incrementScore = this.incrementScore.bind(this);
+  }
+
+  decrementScore(){
+    this.setState({
+      score : this.state.score - 1,
+    });
+  }
+
+  incrementScore(){
+    this.setState({
+      score : this.state.score + 1,
+    });
+  }
+
+  render(){
+    return(
+      <div className="counter">
+          <button onClick={this.decrementScore} className="counter-action decrement">-</button>
+          <div className="counter-score">{this.state.score}</div>
+          <button onClick={this.incrementScore} className="counter-action increment">+</button>
+      </div>
+    );
+  }
+
+}
+
+
+function Player(props){
+  return(
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter initialScore={props.initialScore}/>
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes ={
+  name : PropTypes.string.isRequired,
+  score : PropTypes.number.isRequired
+}
+
+
 function Application(props){
   return(
     <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+      <Header title={props.title}/>
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Shehan Disanayake
-          </div>
-          <div className="player-score">
-            <div className="counter">
-                <button className="counter-action decrement">-</button>
-                <div className="counter-score">31</div>
-                <button className="counter-action increment">+</button>
-            </div>
-          </div>
-        </div>
+        {props.players.map((player)=>{
+          return <Player key={player.id} name={player.name} initialScore={player.score}/>
+        })}
+
       </div>
     </div>
   );
@@ -32,13 +118,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Application title="My Scoreboard"/>
+          <Application title="My Scoreboard" players={players}/>
       </div>
     );
   }
 }
 Application.propTypes ={
-  title : PropTypes.string.isRequired,
+  title : PropTypes.string,
+  players : PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    score : PropTypes.number.isRequired,
+    id : PropTypes.number.isRequired
+  })).isRequired,
+
+};
+Application.defaultProps ={
+  title : "Scoreboard",
 };
 
 export default App;
